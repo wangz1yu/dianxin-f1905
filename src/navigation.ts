f51597a173af280c03b69eab652316bf47dbe213
wangz1yu <49710008+wangz1yu@users.noreply.github.com>
2025-04-07 13:37:03 +0800

    修改底部链接

--- src/navigation.ts
+++ src/navigation.ts
@@ -72,63 +72,50 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: '操作系统',
+      title: '产品服务',
       links: [
-        { text: '点薪·结算系统', href: 'https://www.dianxingg.cn/' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: '智能结算系统', href: 'https://www.dianxingg.cn/' },
+        { text: '灵工签约系统', href: 'https://demo.dianxin.love/services' },
+        { text: '税优服务商', href: 'https://demo.dianxin.love/pricing' },
       ],
     },
     {
-      title: 'Platform',
+      title: '平台',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        { text: '开发人员 API', href: 'https://beta.dianxingg.cn/platform/v2/index.html' },
+        { text: '开放平台', href: '#' },
       ],
     },
     {
-      title: 'Support',
+      title: '解决方案',
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
+        { text: '物流配送', href: 'https://demo.dianxin.love/landing/lead-generation' },
+        { text: '餐饮零售', href: 'https://demo.dianxin.love/landing/sales' },
+        { text: '互联网科技', href: 'https://demo.dianxin.love/landing/click-through' },
+        { text: '金融保险', href: 'https://demo.dianxin.love/landing/product' },
+        { text: '医疗健康', href: 'https://demo.dianxin.love/landing/pre-launch' },
+        { text: '建筑工程', href: 'https://demo.dianxin.love/landing/subscription' },
       ],
     },
     {
-      title: 'Company',
+      title: '关于我们',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: '点薪介绍', href: 'https://demo.dianxin.love/about' },
+        { text: '联系我们', href: 'https://demo.dianxin.love/contact' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: '系统公告', href: getPermalink('/terms') },
   ],
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://dx.imgix.net/logo32.png" alt="点薪logo" loading="lazy"></img>
+     <a class="text-blue-600 underline dark:text-muted" href="https://beian.miit.gov.cn/"> 皖ICP备2024034333号-2</a> · 点薪云.
   `,
 };
